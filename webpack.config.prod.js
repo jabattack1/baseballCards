ea7b27b1b8eddf0dbc17f725d77c2f65684e314c
jabattack1 <peter.hahn@live.com>
2019-05-12 11:56:57 -0400

    css

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -34,11 +34,17 @@ module.exports = {
       include: path.join(__dirname, 'client')
     },
     // CSS
-    { 
-      test: /\.styl$/, 
-      include: path.join(__dirname, 'client'),
-      loader: 'style-loader!css-loader!stylus-loader'
+    {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
     }
     ]
   }
 };
+
+
+
+
+
+
+
